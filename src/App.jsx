f58173514147dca9bf71b08df3375eb19ec34f6a
Breshly Abanid <breshlyabanid@gmail.com>
2024-06-30 21:02:44 +0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>test paragraph</p>
+      <p>test paragraph</p>
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      container: {
+        center: true,
+      },
+    //   extend: {
+    //     fontFamily: {
+    //       poppins: ["Poppins", "sans-serif"],
+    //     },
+    //     colors: {
+
+    //     },
+    //   },
+    },
+    plugins: [],
+  }
+  
+  
