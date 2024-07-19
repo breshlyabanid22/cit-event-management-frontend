78d5a18b37dd235ae090ea67de850cf7f6db7c61
Momoyaan <ianmathewaviso@gmail.com>
2024-07-19 18:19:31 +0800

    feat: (admin) add event-management page

---
+++ src/pages/app/admin/event-management.tsx
@@ -0,0 +1,13 @@
+export default function EventManagement() {
+    return (
+        <div>
+            <header className="mb-6 flex w-full items-center justify-between">
+                <div className="flex flex-col">
+                    <p className="text-3xl font-bold">User Management</p>
+                    <p className="text-md font-light">Manage your users</p>
+                </div>
+            </header>
+            <body className="grid grid-cols-1 gap-4">Hey</body>
+        </div>
+    );
+}
