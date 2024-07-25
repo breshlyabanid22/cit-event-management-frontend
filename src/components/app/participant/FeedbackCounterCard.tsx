8170834d923f835fa4a65f424020523e1bfb2b68
Ian Mathew M. Aviso <ianmathewaviso@gmail.com>
2024-07-25 10:52:16 +0800

    chore: dashboard cards

--- src/components/app/participant/FeedbackCounterCard.tsx
+++ src/components/app/participant/FeedbackCounterCard.tsx
@@ -4,7 +4,12 @@ export default function TotalEventAttended() {
     return (
         <Card className="flex flex-col gap-4 p-10">
             <CardBody className="flex flex-row justify-between items-center self-center ">
-                <p className="text-2xl  text-primary-500">Feedback</p>
+                <div>
+                    <p className="text-5xl font-bold text-warning-500">
+                        0
+                    </p>
+                    <p className="text-2xl  text-primary-500">Feedback</p>
+                </div>
                 <IconBrandHipchat size={50} className="text-primary-500" />
             </CardBody>
         </Card>
--- src/components/app/participant/TotalEventAttended.tsx
+++ src/components/app/participant/TotalEventAttended.tsx
@@ -4,9 +4,14 @@ export default function TotalEventAttended() {
     return (
         <Card className="flex flex-col gap-4 p-10">
             <CardBody className="flex flex-row justify-between items-center self-center ">
-                <p className="text-2xl  text-primary-500">
-                    Total Events Attended
-                </p>
+                <div>
+                    <p className="text-5xl font-bold text-warning-500">
+                        0
+                    </p>
+                    <p className="text-2xl  text-primary-500">
+                        Total Events Attended
+                    </p>
+                </div>
                 <IconCalendarWeek size={50} className="text-primary-500" />
             </CardBody>
         </Card>
